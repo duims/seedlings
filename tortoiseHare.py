691303b8e004e4abb79f10587dafeda436b14330
Julia Duimovich <duimovich.julia@gmail.com>
2014-02-04 22:00:37 -0500

    Adding basic helper functions for linked lists

---
+++ tortoiseHare.py
@@ -0,0 +1,7 @@
+"""Practising really understanding implementing algorithm concepts == actually implementing some algorithms
+This algorithm was invented in the late 60's by Robert Floyd, a computer science professor at Stanford. 
+
+implementation of Linked Lists in python: Each node is a dict containing key: data, the value in the node, and 
+"""
+def tortoiseHare():
+	
